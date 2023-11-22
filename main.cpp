20bffc51c980ba2968cfd516806a30d07aa62381
Krane <freesme@163.com>
2023-11-22 14:09:17 +0800

    update about point

--- main.cpp
+++ main.cpp
@@ -23,5 +23,33 @@ int main() {
     Func::swaps(a, b);
     Func::foo("swaps: ", a, b);
 
+
+    int var1;
+    char var2[10];
+
+    cout << "var1 变量的地址： ";
+    cout << &var1 << endl;
+
+    cout << "var2 变量的地址： ";
+    cout << &var2 << endl;
+
+
+    int var = 20;   // 实际变量的声明
+    int *ip;        // 指针变量的声明
+
+    ip = &var;       // 在指针变量中存储 var 的地址
+
+    cout << "Value of var variable: ";
+    cout << var << endl;
+
+    // 输出在指针变量中存储的地址
+    cout << "Address stored in ip variable: " << endl;
+    cout << ip << endl;
+    cout << &var << endl;
+    // 访问指针中地址的值
+    cout << "Value of *ip variable: ";
+    cout << *ip << endl;
+    cout << &ip << endl;
+
     return 0;
 }
